2bc1086f3debebe65e3fc82847e6243797a1db22
derpycode <andy@seao2.org>
2024-09-10 12:34:07 -0700

    aded simplier test code -- calc_carbchem_user_simple.f90

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 
-FBJS_calc_carbchem_user = \
+FBJS_calc_carbchem_user_simple = \
                     ../ctoaster.carrotcake/src/common/gem_cmn.f90 \
                     ../ctoaster.carrotcake/src/common/gem_util.f90 \
                     ../ctoaster.carrotcake/src/common/gem_carbchem.f90 \
-                    calc_carbchem_user.f90
+                    calc_carbchem_user_simple.f90
 
 #### fortran compiler command name
 FC = gfortran
@@ -13,6 +13,6 @@ FLAGS = -fdefault-real-8 -O2 -O3 -funroll-loops -msse
 #FLAGS += -g -ffpe-trap=zero,overflow,invalid -O0 -Wall -fbounds-check
 
 calc_carbchem_user: 
-	$(FC) $(FLAGS) $(FBJS_calc_carbchem_user) -o calc_carbchem_user.exe
+	$(FC) $(FLAGS) $(FBJS_calc_carbchem_user_simple) -o testcarrot.exe
 clean: 
 	rm -f *.o *.mod *.exe
---
+++ calc_carbchem_user_simple.f90
@@ -0,0 +1,253 @@
+! **********************************************************************************************************************************
+! calc_carbchem_user_simple.f90
+! CARBONATE CHEMISTRY TEST HARNESS -- ASSUMED INPUT + SIMPLIFIED OUTPUT (+ ADDITIONAL COMMENTING)
+! **********************************************************************************************************************************
+
+
+PROGRAM calc_carbchem_user
+
+
+  USE gem_carbchem ! module containing primary subroutines used here
+  USE gem_cmn      ! module contains array index strings and constants that are used here
+  USE gem_util     ! module contains routines needed by gem_carbchem, and directly here e.g., fun_calc_isotope_fraction
+  IMPLICIT NONE
+  SAVE
+
+  ! -------------------------------------------------------- !
+  ! DEFINE VARIABLES
+  ! -------------------------------------------------------- !
+  ! these are arrays passed to/from subroutines
+  ! array size variables (e.g., n_ocn) are parameters with values defined in gem_cmn
+  REAL,DIMENSION(n_ocn)::loc_ocn                                       ! ocean tracers 
+  REAL,DIMENSION(n_carb)::loc_carb                                     ! carbonate chemistry 
+  REAL,DIMENSION(n_carbconst)::loc_carbconst                           ! carbonate chemistry constants
+  REAL,DIMENSION(n_carbalk)::loc_carbalk
+  REAL,DIMENSION(n_carbisor)::loc_carbisor
+  ! these are all variables local to this routine (as opposed to variable defined in e.g., gem_cmn)
+  real::loc_var
+  REAL::loc_D
+  real::loc_DIC_13C,loc_CO2_13C,loc_HCO3_13C,loc_CO3_13C
+  real::loc_DIC_14C,loc_CO2_14C,loc_HCO3_14C,loc_CO3_14C
+  CHARACTER(len=16)::loc_str
+  ! -------------------------------------------------------- !
+  ! INITIALIZE VARIABLES
+  ! -------------------------------------------------------- !
+  ! in FORTRAN, we are grown-ups
+  loc_ocn(:) = 0.0
+  loc_carb(:) = 0.0
+  loc_carbconst(:) = 0.0
+  loc_carbalk(:) = 0.0
+  loc_carbisor(:) = 0.0
+  ! these determine the behavour of the calculation of pH in gem_carbchem
+  par_carbchem_pH_iterationmax = 100
+  par_carbchem_pH_tolerance = 0.001
+  ! -------------------------------------------------------- !
+  ! USER INPUTS
+  ! -------------------------------------------------------- !
+  ! NOTE: ensure units are converted before passing ot dissolution function;
+  !       ocean depth: m
+  !       dCO3 concentration: mol kg-1
+  !       calcite content: normalized (max = 1.0)
+  !       organic carbon rain flux: mol cm-2 yr-1
+  !       dissolved O2 concentration: mol kg-1
+  ! NOTE: the interactive version is commented out in this version
+  ! temperature
+!!$  PRINT*,"Input temperature (degrees C) [RETURN to accept: 10.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_T) = 10.0
+!!$     print*,loc_ocn(io_T),' degrees C'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_T)
+!!$  end if
+  loc_ocn(io_T) = 1.0*loc_ocn(io_T) + const_zeroC
+  ! salinity
+!!$  PRINT*,"Input salinity (o/oo) [RETURN to accept: 34.5]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_S) = 34.5
+!!$     print*,loc_ocn(io_S),' o/oo'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_S)
+!!$  end if
+!!$  loc_ocn(io_S) = 1.0*loc_ocn(io_S)
+  ! ocean depth
+!!$  PRINT*,"Input water column depth (m) [RETURN to accept: 0.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_D = 0.0
+!!$     print*,loc_D,' m'
+!!$  else
+!!$     READ(loc_str, *) loc_D
+!!$  end if
+  loc_D = 1.0*loc_D
+  ! DIC
+!!$  PRINT*,"Input [DIC] (umol kg-1) [RETURN to accept: 2200.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_DIC) = 2200.0
+!!$     print*,loc_ocn(io_DIC),' umol kg-1'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_DIC)
+!!$  end if
+  loc_ocn(io_DIC) = 1.0E-06*loc_ocn(io_DIC)
+  ! ALK
+!!$  PRINT*,"Input [ALK] (umol kg-1) [RETURN to accept: 2300.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_ALK) = 2300.0
+!!$     print*,loc_ocn(io_ALK),' umol kg-1'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_ALK)
+!!$  end if  
+  loc_ocn(io_ALK) = 1.0E-06*loc_ocn(io_ALK)
+  ! phosphate
+!!$  PRINT*,"Input [PO4] (umol kg-1) [RETURN to accept: 0.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_PO4) = 0.0
+!!$     print*,loc_ocn(io_PO4),' umol kg-1'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_PO4)
+!!$  end if
+  loc_ocn(io_PO4) = 1.0E-06*loc_ocn(io_PO4)
+  ! dissolved silica
+!!$  PRINT*,"Input [SiO2] (umol kg-1) [RETURN to accept: 0.0]"
+!!$  READ(*,'(A)') loc_str
+!!$  if (loc_str == '') then
+     loc_ocn(io_SiO2) = 0.0
+!!$     print*,loc_ocn(io_SiO2),' umol kg-1'
+!!$  else
+!!$     READ(loc_str, *) loc_ocn(io_SiO2)
+!!$  end if
+  loc_ocn(io_SiO2) = 1.0E-06*loc_ocn(io_SiO2)
+  ! -------------------------------------------------------- !
+  ! SOLVE pH IN SEAWATER
+  ! -------------------------------------------------------- !
+  ! NOTE: the various subroutines called take some inputs as 'in' only and some as 'inout'
+  !       refer to the subroutines themselves in gem_carbchem
+  ! seed carbonate chemistry calculation with a 'reasonable' H+ concentration (pH)
+  loc_carb(ic_H) = 10**(-7.8)          
+  ! lets have a little H2S and NH4!
+  loc_ocn(io_H2S) = 1.0E-9
+  loc_ocn(io_NH4) = 1.0E-9
+  ! calculate carbonate chemistry constants -- from gem_carbchem
+  call sub_calc_carbconst( &
+       & loc_D,            &
+       & loc_ocn(io_T),    &
+       & loc_ocn(io_S),    &
+       & loc_carbconst(:)  &
+       & )
+  ! estimate Ca and borate etc concentrations from salinity -- all from gem_carbchem
+  loc_ocn(io_Ca)  = fun_calc_Ca(loc_ocn(io_S))
+  loc_ocn(io_B)   = fun_calc_Btot(loc_ocn(io_S))
+  loc_ocn(io_SO4) = fun_calc_SO4tot(loc_ocn(io_S))
+  loc_ocn(io_F)   = fun_calc_Ftot(loc_ocn(io_S))
+  ! calculate carbonate chemistry system -- from gem_carbchem
+  CALL sub_calc_carb(      &
+       & loc_ocn(io_DIC),  &
+       & loc_ocn(io_ALK),  &
+       & loc_ocn(io_Ca),   &
+       & loc_ocn(io_PO4),  &
+       & loc_ocn(io_SiO2), &
+       & loc_ocn(io_B),    &
+       & loc_ocn(io_SO4),  &
+       & loc_ocn(io_F),    &
+       & loc_ocn(io_H2S),  &
+       & loc_ocn(io_NH4),  &
+       & loc_carbconst(:), & 
+       & loc_carb(:),      &
+       & loc_carbalk(:)    & 
+       & )
+  loc_DIC_13C = fun_calc_isotope_fraction(0.0,const_standards(11))*loc_ocn(io_DIC) ! from: gem_util
+  call sub_calc_carb_r13C( &
+       & loc_ocn(io_T), &
+       & loc_ocn(io_DIC), &
+       & loc_DIC_13C, &
+       & loc_carb(:), &
+       & loc_carbisor(:) &
+       & )
+  loc_DIC_14C = fun_calc_isotope_fraction(0.0,const_standards(12))*loc_ocn(io_DIC) ! from: gem_util
+  call sub_calc_carb_r14C( &
+       & loc_ocn(io_T), &
+       & loc_ocn(io_DIC), &
+       & loc_DIC_14C, &
+       & loc_carb(:), &
+       & loc_carbisor(:) &
+       & )
+  loc_DIC_13C  = loc_carbisor(ici_DIC_r13C)*loc_ocn(io_DIC)
+  loc_CO2_13C  = loc_carbisor(ici_CO2_r13C)*loc_carb(ic_conc_CO2)
+  loc_HCO3_13C = loc_carbisor(ici_HCO3_r13C)*loc_carb(ic_conc_HCO3)
+  loc_CO3_13C  = loc_carbisor(ici_CO3_r13C)*loc_carb(ic_conc_CO3)
+  loc_DIC_14C  = loc_carbisor(ici_DIC_r14C)*loc_ocn(io_DIC)
+  loc_CO2_14C  = loc_carbisor(ici_CO2_r14C)*loc_carb(ic_conc_CO2)
+  loc_HCO3_14C = loc_carbisor(ici_HCO3_r14C)*loc_carb(ic_conc_HCO3)
+  loc_CO3_14C  = loc_carbisor(ici_CO3_r14C)*loc_carb(ic_conc_CO3)
+  ! -------------------------------------------------------- !
+  ! PRINT RESULTS
+  ! -------------------------------------------------------- !
+  ! NOTE: shortened to some key indicative info
+  PRINT*,'--- INPUTS ---'
+  PRINT*,'T          = ',loc_ocn(io_T) - const_zeroC,' degrees C'
+  PRINT*,'S          = ',loc_ocn(io_S),' o/oo'
+  PRINT*,'D          = ',loc_D,' m'
+  PRINT*,'ALK        = ',loc_ocn(io_ALK)*1.0E+06,' umol kg-1'
+  PRINT*,'DIC        = ',loc_ocn(io_DIC)*1.0E+06,' umol kg-1'
+  PRINT*,'PO4        = ',loc_ocn(io_PO4)*1.0E+06,' umol kg-1'
+  PRINT*,'SiO2       = ',loc_ocn(io_SiO2)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'--- DERIVED INPUTS ---'
+!!$  PRINT*,'Ca         = ',loc_ocn(io_Ca)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'B          = ',loc_ocn(io_B)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'SO4        = ',loc_ocn(io_SO4)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'F          = ',loc_ocn(io_F)*1.0E+06,' umol kg-1'
+  PRINT*,'--- BASIC CARBONATE PROPERTIES ---'
+  PRINT*,'pH(SWS)    = ',-log10(loc_carb(ic_H)),' '
+!!$  PRINT*,'fCO2       = ',loc_carb(ic_fug_CO2)*1.0E+06,' uatm'
+!!$  PRINT*,'[CO2]      = ',loc_carb(ic_conc_CO2)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'[HCO3-]    = ',loc_carb(ic_conc_HCO3)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'[CO3-]     = ',loc_carb(ic_conc_CO3)*1.0E+06,' umol kg-1'
+  PRINT*,'ohm_cal    = ',loc_carb(ic_ohm_cal),' '
+  PRINT*,'ohm_arg    = ',loc_carb(ic_ohm_arg),' '
+  PRINT*,'--- CARBONATE ALKALINITY CONTRIBUTIONS ---'
+  PRINT*,'Boron alk  = ',loc_carbalk(ica_H4BO4)*1.0E+06,' umol kg-1'
+  PRINT*,'OH alk     = ',loc_carbalk(ica_OH)*1.0E+06,' umol kg-1'
+  PRINT*,'pTOT alk   = ',(loc_carbalk(ica_HPO4) + loc_carbalk(ica_PO4) + loc_carbalk(ica_H3PO4))*1.0E+06,' umol kg-1'
+  PRINT*,'H3SiO4 alk = ',loc_carbalk(ica_H3SiO4)*1.0E+06,' umol kg-1'
+  PRINT*,'HPO4 alk   = ',loc_carbalk(ica_HPO4)*1.0E+06,' umol kg-1'
+  PRINT*,'PO4 alk    = ',loc_carbalk(ica_PO4)*1.0E+06,' umol kg-1'
+  PRINT*,'NH3 alk    = ',loc_carbalk(ica_NH3)*1.0E+06,' umol kg-1'
+  PRINT*,'HS alk     = ',loc_carbalk(ica_HS)*1.0E+06,' umol kg-1'
+  PRINT*,'HSO4 alk   = ',loc_carbalk(ica_HSO4)*1.0E+06,' umol kg-1'
+  PRINT*,'HF alk     = ',loc_carbalk(ica_HF)*1.0E+06,' umol kg-1'
+  PRINT*,'H3PO4 alk  = ',loc_carbalk(ica_H3PO4)*1.0E+06,' umol kg-1'
+!!$  PRINT*,'--- DISSOCIATION CONSTANTS ---'
+!!$  PRINT*,'pk1        = ',-log10(loc_carbconst(icc_k1)),' '
+!!$  PRINT*,'pk2        = ',-log10(loc_carbconst(icc_k2)),' '
+!!$  PRINT*,'pkW        = ',-log10(loc_carbconst(icc_kW)),' '
+!!$  PRINT*,'pkB        = ',-log10(loc_carbconst(icc_kB)),' '
+!!$  PRINT*,'pkSi       = ',-log10(loc_carbconst(icc_kSi)),' '
+!!$  PRINT*,'pkHF       = ',-log10(loc_carbconst(icc_kHF)),' '
+!!$  PRINT*,'pkHSO4     = ',-log10(loc_carbconst(icc_kHSO4)),' '
+!!$  PRINT*,'pkP1       = ',-log10(loc_carbconst(icc_kP1)),' '
+!!$  PRINT*,'pkP2       = ',-log10(loc_carbconst(icc_kP2)),' '
+!!$  PRINT*,'pkP3       = ',-log10(loc_carbconst(icc_kP3)),' '
+!!$  PRINT*,'pkH2S      = ',-log10(loc_carbconst(icc_kH2S)),' '
+!!$  PRINT*,'pkNH4      = ',-log10(loc_carbconst(icc_kNH4)),' '
+!!$  PRINT*,'pkcal      = ',-log10(loc_carbconst(icc_kcal)),' '
+!!$  PRINT*,'pkarg      = ',-log10(loc_carbconst(icc_karg)),' '
+  PRINT*,'--- ISOTOPIE PROPERTIES ---'
+  PRINT*,'DIC_d13C  = ',fun_calc_isotope_delta(loc_ocn(io_DIC),loc_DIC_13C,const_standards(11),.true.,const_nulliso),' o/oo'
+  PRINT*,'CO2_d13C  = ',fun_calc_isotope_delta(loc_carb(ic_conc_CO2),loc_CO2_13C,const_standards(11),.true.,const_nulliso),' o/oo'
+!!$  PRINT*,'HCO3_d13C = ',fun_calc_isotope_delta(loc_carb(ic_conc_HCO3),loc_HCO3_13C,const_standards(11),.true.,const_nulliso),' o/oo'
+!!$  PRINT*,'CO3_d13C  = ',fun_calc_isotope_delta(loc_carb(ic_conc_CO3),loc_CO3_13C,const_standards(11),.true.,const_nulliso),' o/oo'
+  PRINT*,'DIC_d14C  = ',fun_calc_isotope_delta(loc_ocn(io_DIC),loc_DIC_14C,const_standards(12),.true.,const_nulliso),' o/oo'
+  PRINT*,'CO2_d14C  = ',fun_calc_isotope_delta(loc_carb(ic_conc_CO2),loc_CO2_14C,const_standards(12),.true.,const_nulliso),' o/oo'
+!!$  PRINT*,'HCO3_d14C = ',fun_calc_isotope_delta(loc_carb(ic_conc_HCO3),loc_HCO3_14C,const_standards(12),.true.,const_nulliso),' o/oo'
+!!$  PRINT*,'CO3_d14C  = ',fun_calc_isotope_delta(loc_carb(ic_conc_CO3),loc_CO3_14C,const_standards(12),.true.,const_nulliso),' o/oo'
+  PRINT*,'--- END ---'
+  PRINT*,' '
+  ! -------------------------------------------------------- !
+  ! END
+  ! -------------------------------------------------------- !
+  
+END PROGRAM calc_carbchem_user
